be9095d671bbcc27081963b0868581f850a14b3d
rajshekharrr <116083610+rajshekharrr@users.noreply.github.com>
2022-12-24 02:07:21 +0530

    Update main.py
    tf slim updated

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ np.random.seed(42)
 rn.seed(12345)
 tf.random.set_seed(1234)
 
-import tensorflow.contrib.slim as slim
+import tf_slim as slim
 import sys, shutil, subprocess
 
 from lib.ops import *
